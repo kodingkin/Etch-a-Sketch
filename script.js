bc16d908b4119bb38c2ef6b83fdb7a36f06b8549
kodingkin <wankin527@gmail.com>
2025-04-05 12:43:20 +0900

    add some css on the button

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 // get the container from the itml
-const body = document.querySelector("body");
 const gridContainer = document.querySelector("#grid_container");
+const buttonContainer = document.querySelector("#button_container");
 gridContainer.style.width = "100%";
 
 let sideLength = 16;
-const createGrids = function(sideLength) {
+const createGrids = function(sideLength) { 
     // create 16x16 grids
     for (let i = 0; i < sideLength; i++) {
         const container = document.createElement("div");
         container.classList.add("container");
         container.style.display = "flex";
-        container.style.width = "48%";
+        container.style.width = "70%";
         container.style.flexDirection = "row";
 
         for (let g = 0; g < sideLength; g++) {
             // create the grid
             const grid = document.createElement("div");
+            const gridWidth = 100/sideLength;
+            const gridWidthString = gridWidth.toString() + "%"; 
+            console.log(gridWidthString);
 
             // add the classes for gird
             grid.classList.add("grid");
@@ -26,8 +29,8 @@ const createGrids = function(sideLength) {
             grid.classList.add(gridrow);
 
             // style for grid
-            grid.style.width = "6%";
-            grid.style.paddingTop = "6%";
+            grid.style.width = gridWidthString;
+            grid.style.paddingTop = gridWidthString;
             grid.style.backgroundColor = "skyblue";
             grid.style.flexShrink = "0";
             grid.addEventListener("mouseover", function (event) {
@@ -46,10 +49,10 @@ createGrids(sideLength);
 const createGridButton = document.createElement("button");
 createGridButton.innerText = "Create a new Grid"
 createGridButton.addEventListener("click", function () {
-    const input = prompt("Please input the resolution of the drawing pad");
+    const input = prompt("Please input the resolution of the drawing pad (1-100)");
     const sideLength = parseInt(input);
-    if (sideLength === null || isNaN(sideLength)) {
-        alert("plese enter a number")
+    if (sideLength === null || isNaN(sideLength) || sideLength > 100 || sideLength <= 0) {
+        alert("plese enter a number between 1-100")
         return;
     }
     const containers = document.querySelectorAll(".container");
@@ -58,7 +61,7 @@ createGridButton.addEventListener("click", function () {
     })
     createGrids(sideLength);
 })
-document.body.insertBefore(createGridButton, gridContainer)
+buttonContainer.appendChild(createGridButton);
 
 // create reset button
 const resetButton = document.createElement("button");
@@ -69,4 +72,4 @@ resetButton.addEventListener("click", function () {
         grid.style.backgroundColor = "skyblue";
     });
 })
-document.body.insertBefore(resetButton, gridContainer)
+buttonContainer.appendChild(resetButton);
